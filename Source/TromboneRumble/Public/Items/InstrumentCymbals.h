2c4685d9792d5ae256d94c0f3157016d2b9e89dd
YmSong <ssupersam13@gmail.com>
2026-01-24 01:27:38 +0900

    feat : 심벌즈 애니메이션 추가

--- Source/TromboneRumble/Public/Items/InstrumentCymbals.h
+++ Source/TromboneRumble/Public/Items/InstrumentCymbals.h
@@ -7,7 +7,7 @@
 #include "InstrumentCymbals.generated.h"
 
 /**
- * 
+ * 심벌즈는 반쪽짜리로 맵에 스폰되기 때문에 장착시 남은 반쪽을 생성후, 플레이어의 오른손에 달아줘야함.
  */
 UCLASS(Abstract)
 class TROMBONERUMBLE_API AInstrumentCymbals : public AInstrumentBase
@@ -17,5 +17,12 @@ public:
 	virtual void Multicast_OnHitSuccess_Implementation(AActor* HitActor) override;
 	virtual void Client_OnHitSuccess_Implementation(AActor* HitActor) override;
 protected:
+	virtual void OnRep_CurrentOwner(AActor* OldActor) override;
 	virtual float CalculateScore(ENoteResult InNoteResult, int32 CurrentCombo) override;
+
+	UPROPERTY(EditDefaultsOnly , Category = "Config")
+	TSubclassOf<AActor> CymbalsHalfClass = nullptr;
+
+	UPROPERTY(Transient)
+	TObjectPtr<AActor> CymbalsRightHandActor = nullptr;
 };
