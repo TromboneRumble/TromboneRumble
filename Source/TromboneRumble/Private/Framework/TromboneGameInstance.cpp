bb51115aab3ec59b96e6cc5c1cb5c0389d883645
JeongJongMun <whdans4005@naver.com>
2026-01-24 02:45:46 +0900

    feat : 메뉴 브금이 메인/매치/로비에서 들리고 인게임에서 꺼지게 추가

--- Source/TromboneRumble/Private/Framework/TromboneGameInstance.cpp
+++ Source/TromboneRumble/Private/Framework/TromboneGameInstance.cpp
@@ -1,5 +1,48 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Framework/TromboneGameInstance.h"
+#include "AkComponent.h"
 
+void UTromboneGameInstance::Init()
+{
+	Super::Init();
+	
+	PlayMenuBGM();
+}
+
+void UTromboneGameInstance::PlayMenuBGM()
+{
+	if (bIsMenuMusicPlaying || PlayMenuBGMEvents.Num() == 0) return;
+
+	if (!GlobalBGMComponent)
+	{
+		GlobalBGMComponent = NewObject<UAkComponent>(this);
+		GlobalBGMComponent->RegisterComponentWithWorld(GetWorld());
+	}
+	
+	TArray<EMenuBGMType> Keys;
+	PlayMenuBGMEvents.GetKeys(Keys);
+	
+	EMenuBGMType SelectedType = Keys[FMath::RandRange(0, Keys.Num() - 1)];
+	if (UAkAudioEvent* PlayEvent = PlayMenuBGMEvents[SelectedType])
+	{
+		if (!GlobalBGMComponent->HasActiveEvents())
+		{
+			GlobalBGMComponent->PostAkEvent(PlayEvent);
+			CurrentMenuBGMType = SelectedType;
+			bIsMenuMusicPlaying = true;
+		}
+	}
+}
+
+void UTromboneGameInstance::StopMenuBGM()
+{
+	if (!GlobalBGMComponent || CurrentMenuBGMType == EMenuBGMType::None) return;
+
+	if (TObjectPtr<UAkAudioEvent>* StopEventPtr = StopMenuBGMEvents.Find(CurrentMenuBGMType))
+	{
+		GlobalBGMComponent->PostAkEvent(*StopEventPtr);
+		bIsMenuMusicPlaying = false;
+		CurrentMenuBGMType = EMenuBGMType::None;
+	}
+}
