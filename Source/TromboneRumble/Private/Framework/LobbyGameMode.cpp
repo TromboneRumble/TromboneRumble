bcfc42fca98ff5ba5351493e27d0fb3aa8f278c5
YmSong <ssupersam13@gmail.com>
2026-03-05 02:03:05 +0900

    feat : 로비 맵에서 방장이 곡 바꿀수 잇는 디버그 기능 추가

--- Source/TromboneRumble/Private/Framework/LobbyGameMode.cpp
+++ Source/TromboneRumble/Private/Framework/LobbyGameMode.cpp
@@ -145,7 +145,7 @@ void ALobbyGameMode::HandlePlayerLoadingScreenFinished(APlayerController* PC)
 			const FGameplayTag SelectedSong = FMath::RandBool() ? TromboneGamePlayTags::Trombone_Rhythm_Song_EasyMapA : 
 																TromboneGamePlayTags::Trombone_Rhythm_Song_EasyMapB;
 			//const FGameplayTag SelectedSong = TromboneGamePlayTags::Trombone_Rhythm_Song_EasyMapB;
-			LobbyGameState->SetSelectedSongTag(SelectedSong);
+			//LobbyGameState->SetSelectedSongTag(SelectedSong);
 			InitializeInstruments();
 		}
 		SetLobbyState(ELobbyState::CountdownToScramble);
