59e7b902fef5c76a63a4f859175ec5be5f3b3bda
JeongJongMun <whdans4005@naver.com>
2026-04-16 21:25:31 +0900

    feat : EscapePopup에 확인 팝업 추가

--- Source/TromboneRumble/Private/UI/UserWidgets/Popup/EscapePopup.cpp
+++ Source/TromboneRumble/Private/UI/UserWidgets/Popup/EscapePopup.cpp
@@ -1,5 +1,6 @@
 #include "UI/UserWidgets/Popup/EscapePopup.h"
 #include "EasyOnlineSession.h"
+#include "Framework/TromboneGameInstance.h"
 #include "UI/UserWidgets/Common/CommonButtonBaseWithText.h"
 #include "Utilities/DebugHelper.h"
 #include "Utilities/TromboneStatics.h"
@@ -32,10 +33,34 @@ void UEscapePopup::HandleOptionButtonClicked()
 
 void UEscapePopup::HandleDisconnectButtonClicked()
 {
-	if (UEasyOnlineSession* OnlineSession = UEasyOnlineSession::Get(this))
+	UTwoButtonWithoutClosePopup* ConfirmPopup = UTromboneStatics::ShowPopup<UTwoButtonWithoutClosePopup>(GetWorld());
+	if (!ConfirmPopup)
 	{
-		OnlineSession->LeaveGameSession();
+		LOG_WITH_CURRENT_CONTEXT(Warning, TEXT("Failed to show disconnect confirmation popup"));
 		return;
+	}
+	
+	if (UTromboneGameInstance* GI = Cast<UTromboneGameInstance>(GetGameInstance()))
+	{
+		// TODO : UI 용 String 테이블 따로 만들어서 관리하기
+		const FText Title = FText::FromString(TEXT("일시정지"));
+		const FText Description = FText::FromString(TEXT("정말로 게임에서 나가시겠습니까?\n현재 매치의 진행 상황과 기록이\n모두 초기화 됩니다."));
+		const FText LeftButtonText = FText::FromString(TEXT("계속하기"));
+		const FText RightButtonText = FText::FromString(TEXT("나가기"));
+		
+		const TFunction<void()> LeftCallback = [this, ConfirmPopup]()
+		{
+			ConfirmPopup->ClosePopup();
+		};
+		const TFunction<void()> RightCallback = [this]()
+		{
+			if (UEasyOnlineSession* OnlineSession = UEasyOnlineSession::Get(this))
+			{
+				OnlineSession->LeaveGameSession();
+			}
+		};
+		
+		ConfirmPopup->OnInit(Title, Description, LeftButtonText, RightButtonText, LeftCallback, RightCallback);
 	}
 	
 	LOG_WITH_CURRENT_CONTEXT(Warning, TEXT("Failed to leave game session"));
