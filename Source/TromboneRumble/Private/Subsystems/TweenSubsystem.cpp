efe70257280b0c8ab63b2c91a55e376427eb28be
JeongJongMun <whdans4005@naver.com>
2026-04-27 23:40:35 +0900

    feat : Toast System & Tween System
    메인 메뉴에서 로비 코드 없이 Join 누르면 토스트 메세지 나오게 넣어둠

---
+++ Source/TromboneRumble/Private/Subsystems/TweenSubsystem.cpp
@@ -0,0 +1,99 @@
+﻿// Copyright (C) 2026 biksari studio. All Rights Reserved.
+
+#include "Subsystems/TweenSubsystem.h"
+#include "Blueprint/UserWidget.h"
+
+void UTweenSubsystem::DoTween(UUserWidget* InWidget, const FVector2D InTargetScale, const float InDuration, const ETweenCurveType InCurveType, const FSimpleDelegate& InCompletionDelegate)
+{
+	if (!InWidget)
+	{
+		return;
+	}
+
+	ActiveTweens.RemoveAll([InWidget](const FTweenRuntimeInfo& Info) {
+		return Info.TargetWidget == InWidget;
+	});
+
+	FTweenRuntimeInfo NewTween;
+	NewTween.TargetWidget = InWidget;
+	NewTween.StartScale = InWidget->GetRenderTransform().Scale;
+	NewTween.TargetScale = InTargetScale;
+	NewTween.Duration = FMath::Max(InDuration, 0.001f); // !division by zero
+	NewTween.CurveType = InCurveType;
+	NewTween.ElapsedTime = 0.f;
+	NewTween.CompletionCallback = InCompletionDelegate;
+
+	ActiveTweens.Add(NewTween);
+}
+
+float UTweenSubsystem::EaseOutBack(float T)
+{
+	const float S = 1.70158f;
+	return (T -= 1.f) * T * ((S + 1.f) * T + S) + 1.f;
+}
+
+float UTweenSubsystem::GetEasedAlpha(const float Alpha, const ETweenCurveType CurveType) const
+{
+	switch (CurveType)
+	{
+	case ETweenCurveType::Linear:
+		return Alpha;
+	case ETweenCurveType::EaseIn:
+		return Alpha * Alpha;
+	case ETweenCurveType::EaseOut:
+		return 1.f - (1.f - Alpha) * (1.f - Alpha);
+	case ETweenCurveType::EaseOutBack:
+		return EaseOutBack(Alpha);
+	default:
+		return Alpha;
+	}
+}
+
+void UTweenSubsystem::OnWorldCleanUp(UWorld* InWorld)
+{
+	ActiveTweens.Empty();
+}
+
+void UTweenSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	
+	FCoreUObjectDelegates::PostLoadMapWithWorld.AddUObject(this, &ThisClass::OnWorldCleanUp);
+}
+
+void UTweenSubsystem::Deinitialize()
+{
+	FCoreUObjectDelegates::PostLoadMapWithWorld.RemoveAll(this);
+	ActiveTweens.Empty();
+	
+	Super::Deinitialize();
+}
+
+void UTweenSubsystem::Tick(const float DeltaTime)
+{
+	for (int32 i = ActiveTweens.Num() - 1; i >= 0; --i)
+	{
+		FTweenRuntimeInfo& Tween = ActiveTweens[i];
+
+		if (!Tween.TargetWidget.IsValid())
+		{
+			ActiveTweens.RemoveAt(i);
+			continue;
+		}
+
+		Tween.ElapsedTime += DeltaTime;
+		const float Alpha = FMath::Clamp(Tween.ElapsedTime / Tween.Duration, 0.f, 1.f);
+        
+		float EasedAlpha = GetEasedAlpha(Alpha, Tween.CurveType);
+
+		const FVector2D CurrentScale = FMath::Lerp(Tween.StartScale, Tween.TargetScale, EasedAlpha);
+		Tween.TargetWidget->SetRenderScale(CurrentScale);
+
+		if (Alpha >= 1.f)
+		{
+			FSimpleDelegate Callback = ActiveTweens[i].CompletionCallback;
+			ActiveTweens.RemoveAt(i);
+			Callback.ExecuteIfBound();
+		}
+	}
+}
---
+++ Source/TromboneRumble/Public/Subsystems/ToastSubsystem.h
@@ -0,0 +1,70 @@
+﻿// Copyright (C) 2026 biksari studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "ToastSubsystem.generated.h"
+
+enum class EToastSystemPolicy : uint8;
+class UToastContainerWidget;
+class UToastItemWidget;
+struct FToastRequest;
+
+UCLASS()
+class TROMBONERUMBLE_API UToastSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	
+	/** Default constructor */
+	UToastSubsystem();
+	
+	/** Show a toast message with the given request data. */
+	UFUNCTION(BlueprintCallable, Category = "UI|Toast")
+	void ShowToast(const FToastRequest& InRequest);
+	
+	/** Return a toast widget to the pool */
+	void ReturnToPool(UToastItemWidget* ToastWidget);
+
+private:
+	
+	/** @return toast widget from the pool or create a new one */
+	UToastItemWidget* GetOrCreateToastWidget();
+	
+	void ProcessNextToastInternal(const FToastRequest& InRequest);
+	
+	void ProcessNextToast();
+
+private:
+	
+	UPROPERTY()
+	TSubclassOf<UToastItemWidget> SimpleToastItemClass;
+	
+	UPROPERTY()
+	TSubclassOf<UToastContainerWidget> ToastContainerClass;
+	
+private:
+	
+	UPROPERTY()
+	TObjectPtr<UToastContainerWidget> ToastContainer;
+	
+	UPROPERTY()
+	TArray<TObjectPtr<UToastItemWidget>> ToastPool;
+	
+	TQueue<FToastRequest> ToastQueue;
+	
+	/** Queue or override */
+	EToastSystemPolicy ToastPolicy;
+	
+	bool bIsToastShowing;
+	
+public:
+	
+	// ~ Begin USubsystem interface
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+	// ~ End USubsystem interface
+	
+};
