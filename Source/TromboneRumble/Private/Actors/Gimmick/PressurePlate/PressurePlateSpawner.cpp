8ddb23d2f76a8d85530286d3f0cc0cda5acbcbed
YmSong <ssupersam13@gmail.com>
2026-05-10 17:45:19 +0900

    fix : 래그돌 롤백 & 심벌즈 엑스레이 현상 수정

--- Source/TromboneRumble/Private/Actors/Gimmick/PressurePlate/PressurePlateSpawner.cpp
+++ Source/TromboneRumble/Private/Actors/Gimmick/PressurePlate/PressurePlateSpawner.cpp
@@ -52,7 +52,6 @@ void APressurePlateSpawner::OnMusicCallbackReceived(EAkCallbackType CallbackType
 			if (CueName == TEXT("Event_Spotlight_Fever"))
 			{
 				bIsFeverTime = true;
-				TriggerPlateSpawn();
 			}
 		}
 	}
@@ -79,11 +78,12 @@ void APressurePlateSpawner::OnPlateDestroyed(AActor* DestroyedActor)
 
 void APressurePlateSpawner::TriggerPlateSpawn()
 {
-	if (!HasAuthority()) return;
-
+	if (!HasAuthority()) return;	
+	if (PressurePlateClass == nullptr || SpawnPoints.IsEmpty() || SpawningActorClasses.IsEmpty()) return;
+	
 	const int32 MaxCount = bIsFeverTime ? MaxPlates_Fever : MaxPlates_Normal;
 
-	if (SpawnMap.Num() < MaxCount)
+	if (!SpawnMap.IsEmpty() && SpawnMap.Num() < MaxCount)
 	{
 		//TriggerPoint중에 사용 가능한 Point반환
 		TArray<TObjectPtr<ATargetPoint>> AvailablePoints;
