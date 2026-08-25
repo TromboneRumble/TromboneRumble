f4a5ca2ae0876bfa936c8616339dd35e8f0e05fc
JeongJongMun <whdans4005@naver.com>
2026-08-26 01:46:55 +0900

    feat : 기존 UCommonButtonBaseExtensionWithText에서 UCommonButtonBaseExtension를 분리하고, PlayModePopup 버튼 3종에 호버 사운드 추가
    여러 버튼을 더 잘 지원하기 위해선 분리하는게 맞는듯

--- Source/TromboneRumble/Private/UI/Styles/CommonButtonStyleExtension.cpp
+++ Source/TromboneRumble/Private/UI/Styles/CommonButtonStyleExtension.cpp
@@ -1,5 +1,20 @@
-// Copyright (C) 2026 biksari studio. All Rights Reserved.
-
+﻿// Copyright (C) 2026 biksari studio. All Rights Reserved.
 
 #include "UI/Styles/CommonButtonStyleExtension.h"
+#include "AkGameplayStatics.h"
 
+void UCommonButtonStyleExtension::PostHoveredSound(AActor* SoundOwner) const
+{
+	if (NormalHoveredAudioEvent)
+	{
+		UAkGameplayStatics::PostEvent(NormalHoveredAudioEvent, SoundOwner, 0, FOnAkPostEventCallback());
+	}
+}
+
+void UCommonButtonStyleExtension::PostPressedSound(AActor* SoundOwner) const
+{
+	if (NormalPressedAudioEvent)
+	{
+		UAkGameplayStatics::PostEvent(NormalPressedAudioEvent, SoundOwner, 0, FOnAkPostEventCallback());
+	}
+}
---
+++ Source/TromboneRumble/Private/UI/UserWidgets/Common/CommonButtonBaseExtension.cpp
@@ -0,0 +1,55 @@
+﻿// Copyright (C) 2026 biksari studio. All Rights Reserved.
+
+#include "UI/UserWidgets/Common/CommonButtonBaseExtension.h"
+#include "CommonTextBlock.h"
+#include "UI/Styles/CommonButtonStyleExtension.h"
+
+void UCommonButtonBaseExtension::NativeOnHovered()
+{
+	Super::NativeOnHovered();
+	
+	if (const UCommonButtonStyleExtension* StyleExtension = GetStyleExtensionCDO())
+	{
+		StyleExtension->PostHoveredSound(GetOwningPlayerPawn());
+	}
+}
+
+void UCommonButtonBaseExtension::NativeOnPressed()
+{
+	Super::NativeOnPressed();
+	
+	NativeOnCurrentTextStyleChanged();
+	
+	if (const UCommonButtonStyleExtension* StyleExtension = GetStyleExtensionCDO())
+	{
+		StyleExtension->PostPressedSound(GetOwningPlayerPawn());
+	}
+}
+
+void UCommonButtonBaseExtension::NativeOnReleased()
+{
+	Super::NativeOnReleased();
+	
+	NativeOnCurrentTextStyleChanged();
+}
+
+const UCommonButtonStyleExtension* UCommonButtonBaseExtension::GetStyleExtensionCDO() const
+{
+	return Cast<UCommonButtonStyleExtension>(GetStyleCDO());
+}
+
+TSubclassOf<UCommonTextStyle> UCommonButtonBaseExtension::GetDesiredTextStyleClass() const
+{
+	if (IsPressed())
+	{
+		if (const UCommonButtonStyleExtension* StyleExtension = GetStyleExtensionCDO())
+		{
+			if (StyleExtension->NormalPressedTextStyle)
+			{
+				return StyleExtension->NormalPressedTextStyle;
+			}
+		}
+	}
+	
+	return GetCurrentTextStyleClass();
+}
