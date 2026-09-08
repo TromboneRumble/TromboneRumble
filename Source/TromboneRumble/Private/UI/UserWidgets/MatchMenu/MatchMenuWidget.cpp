9e1feefa5f78c3c69866a00585e0c222c5ec3c5a
JeongJongMun <whdans4005@naver.com>
2026-09-08 14:44:12 +0900

    fix : 매치 메뉴 재진입 시 뒤로가기 버튼이 비활성화로 남는 문제 수정

--- Source/TromboneRumble/Private/UI/UserWidgets/MatchMenu/MatchMenuWidget.cpp
+++ Source/TromboneRumble/Private/UI/UserWidgets/MatchMenu/MatchMenuWidget.cpp
@@ -32,15 +32,12 @@ void UMatchMenuWidget::NativeDestruct()
 
 void UMatchMenuWidget::Init()
 {
-	const APlayerController* PC = GetOwningPlayer();
-	if (!PC) return;
-	const bool bIsHost = PC->HasAuthority();
+	if (!GetOwningPlayer()) return;
 	
 	if (CB_Start)
 	{
 		CB_Start->OnClicked().RemoveAll(this);
 		CB_Start->OnClicked().AddUObject(this, &ThisClass::HandleStartButtonClicked);
-		CB_Start->SetIsEnabled(bIsHost);
 	}
 	if (CB_Back)
 	{
@@ -52,15 +49,16 @@ void UMatchMenuWidget::Init()
 		InitMatchTypes();
 		CR_MatchType->OnRotatedWithDirection().RemoveAll(this);
 		CR_MatchType->OnRotatedWithDirection().AddDynamic(this, &ThisClass::HandleOnRotatedMatchType);
-		CR_MatchType->SetInteractionEnabled(bIsHost);
 	}
 	if (CR_Map)
 	{
 		InitSelectableMaps();
 		CR_Map->OnRotatedWithDirection().RemoveAll(this);
 		CR_Map->OnRotatedWithDirection().AddDynamic(this, &ThisClass::HandleOnRotatedMap);
-		CR_Map->SetInteractionEnabled(bIsHost);
 	}
+
+	bIsStarted = false;
+	SetUIEnabled(true);
 	
 	// 게임 스테이트 구독은 로테이터 초기화 뒤여야 한다. 초기 동기화가 위에서 채운 캐시로 인덱스를 찾기 때문
 	CachedMatchMenuGS = GetWorld()->GetGameState<AMatchMenuGameState>();
